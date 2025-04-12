d55676a5031a0f47f8b05e2eac0081a2c7bc9355
emadhashem <eali32028@gmail.com>
2025-04-12 19:23:05 +0200

    save

--- lokta-front/src/api/constants.ts
+++ lokta-front/src/api/constants.ts
@@ -1,4 +1,4 @@
-export const API_URL = "http://localhost:8000";
-// export const API_URL = 'https://lainava.com/back';
-export const IMAGES_API_URL = "http://localhost:8000";
-// export const IMAGES_API_URL = "https://lainava.com/back/public"
+// export const API_URL = "http://localhost:8000";
+export const API_URL = 'https://lainava.com/back';
+// export const IMAGES_API_URL = "http://localhost:8000";
+export const IMAGES_API_URL = "https://lainava.com/back/public"
