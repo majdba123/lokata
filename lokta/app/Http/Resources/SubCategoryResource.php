6fbefe56e8b98c908200e026062dc726bd2f8896
majdba123 <137396563+majdba123@users.noreply.github.com>
2025-05-01 13:12:39 +0300

    Update SubCategoryResource.php
    2

--- lokta/app/Http/Resources/SubCategoryResource.php
+++ lokta/app/Http/Resources/SubCategoryResource.php
@@ -7,11 +7,12 @@ use Illuminate\Http\Request;
 
 class SubCategoryResource extends JsonResource
 {
-    
+
     public function toArray(Request $request): array
     {
         return [
             'id' => $this->id,
+            'category_id' => $this->category_id,
             'title' => $this->title,
             'image' => $this->image,
         ];
