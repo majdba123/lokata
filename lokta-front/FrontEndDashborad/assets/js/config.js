7cfa3044026630735821c776f676b2a895f7bd6e
Mohmadkahal22 <mohmadalkahal@gmail.com>
2025-05-04 03:44:01 -0500

    .
    .

---
+++ lokta-front/FrontEndDashborad/assets/js/config.js
@@ -0,0 +1,11 @@
+// config.js
+window.API_CONFIG = {
+    BASE_URL: 'http://127.0.0.1:8000', 
+    ENDPOINTS: {
+        LOGIN: '/api/login', 
+    },
+    DEFAULT_HEADERS: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+    },
+};
